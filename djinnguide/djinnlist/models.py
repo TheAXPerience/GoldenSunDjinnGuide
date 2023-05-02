8a34a0279d30c3fb740d551b2bb73e6084295825
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-02 16:08:49 -0700

    Update models.py

--- djinnguide/djinnlist/models.py
+++ djinnguide/djinnlist/models.py
@@ -2,4 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Collectable(models.Model):
-    pass
+    name = CharField(
+        max_length=12
+    )
+
+    game = SmallIntegerField()
+
+    coltype = SmallIntegerField()
+
+    chapter = SmallIntegerField()
+
+    location = CharField(
+        max_length=100
+    )
+
+    description = TextField()
+
+    picture = FilePathField()
