42627178f4aea11829895232d7e114b69f92e2f1
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-02 16:49:35 -0700

    Only one URL now

--- djinnguide/djinnlist/urls.py
+++ djinnguide/djinnlist/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:game>', views.by_game, name='by_game'),
-    path('<int:game>/chapter/<int:chapter>', views.by_chapter, name='by_chapter'),
-    path('<int:game>/type/<int:coltype>', views.by_type, name='by_type'),
 ]
