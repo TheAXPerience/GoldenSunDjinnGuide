b708318d3ce53ba3b66e7d3321bb5be2694c3b8d
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-05 18:54:02 -0700

    Update settings.py

--- djinnguide/djinnguide/settings.py
+++ djinnguide/djinnguide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@(795pz1^r_3(d0=ga%zj%3pd^92e3affczjj(ba$@ocoy#yr3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
