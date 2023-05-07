4c6676fafae41b700a09580d143bd46dcc12581c
Alvin Xu <alvin3xu1@gmail.com>
2023-05-07 13:23:39 -0700

    change settings.py to ready for production

--- djinnguide/djinnguide/settings.py
+++ djinnguide/djinnguide/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@(795pz1^r_3(d0=ga%zj%3pd^92e3affczjj(ba$@ocoy#yr3'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-@(795pz1^r_3(d0=ga%zj%3pd^92e3affczjj(ba$@ocoy#yr3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost"]
 
