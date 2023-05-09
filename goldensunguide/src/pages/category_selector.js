b62de2efb4ab6a49d18507e86dd2dff4585c4b6c
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-09 13:53:20 -0700

    Update category_selector.js

--- goldensunguide/src/pages/category_selector.js
+++ goldensunguide/src/pages/category_selector.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export default const CategorySelector = (props) => {
+    return (
+        <section>
+            <h1>{props.game}</h1>
+            Choose a Category
+        </section>
+    );
+}
