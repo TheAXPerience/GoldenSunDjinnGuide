b8e2943d428301fded85e0183434b41e4c56b7a3
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-09 15:34:41 -0700

    Update App.js

--- goldensunguide/src/App.js
+++ goldensunguide/src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home.js';
 import GuidePage from './pages/guidepage.js';
 
 function App() {
   return (
     <Router>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/goldensun' element={<GuidePage game='goldensun' />} />
-            <Route path='thelostage' element={<GuidePage game='thelostage' />} />
-            <Route path='darkdawn' element={<GuidePage game='darkdawn' />} />
-        <Routes />
-    <Router />
+        <Switch>
+            <Route
+                path='/' exact
+                render={(props) => (
+                    <Home {...props} />
+                )}
+            />
+            <Route
+                path='/goldensun'
+                render={(props) => (
+                    <GuidePage {...props} game='goldensun' />
+                )}
+            />
+            <Route
+                path='/thelostage'
+                render={(props) => (
+                    <GuidePage {...props} game='thelostage' />
+                )}
+            />
+            <Route
+                path='/darkdawn'
+                render={(props) => (
+                    <GuidePage {...props} game='darkdawn' />
+                )}
+            />
+        </Switch>
+    </Router>
   );
 }
 
