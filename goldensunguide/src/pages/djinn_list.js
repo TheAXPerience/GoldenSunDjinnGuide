4745e1b17bafd0926baf2071441c4cfbbe17ce7d
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-09 13:56:20 -0700

    Update djinn_list.js

--- goldensunguide/src/pages/djinn_list.js
+++ goldensunguide/src/pages/djinn_list.js
@@ -3,6 +3,9 @@ import DjinnBox from 'djinn_box';
 
 export default const DjinnList = () => {
     return (
-        <h1>Djinn List</h1>
+        <>
+            <h1>Djinn List</h1>
+            <DjinnBox />
+        <>
     );
 }
