4ab51d3d2525df17f2c8a84dce4c8d3a25e43eeb
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-09 13:48:47 -0700

    Update guidepage.js

--- goldensunguide/src/pages/guidepage.js
+++ goldensunguide/src/pages/guidepage.js
@@ -1,3 +1,9 @@
 import React from 'react';
-import CategorySelector from 'category-selector';
+import CategorySelector from 'category_selector';
 import DjinnList from 'djinn_list';
+
+export default const GuidePage = (props) => {
+    return (
+        <p>{props.game}</p>
+    )
+}
