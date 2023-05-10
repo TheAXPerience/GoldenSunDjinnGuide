03b9118fcb03838c00b9e22fb38a462bc280151c
Alvin <57538990+TheAXPerience@users.noreply.github.com>
2023-05-09 18:02:07 -0700

    Update guidepage.js

--- goldensunguide/src/pages/guidepage.js
+++ goldensunguide/src/pages/guidepage.js
@@ -2,9 +2,21 @@ import React from 'react';
 import CategorySelector from 'category_selector';
 import DjinnList from 'djinn_list';
 
+function django_fetch(url) {
+    // fetch data from Django (localhost:8000/api/${url})
+    // parse JSON response to get list
+    // return list
+    return [];
+}
+
 export class GuidePage extends React.Component {
     constructor(props) {
         super(props);
+        // fetch data for CategorySelector
+    }
+
+    componentDidUpdate(prevState) {
+        // if state changed, fetch data for DjinnList
     }
 
     render() {
